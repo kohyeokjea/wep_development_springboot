1a5005e7d1eb5fa960481561c2630098ba0abeaf
kohyeokjea <kohj0686@naver.com>
2025-01-10 20:00:06 +0900

    feat: C making in CRUD

--- chapter06/src/test/java/springbootdeveloper/MemberRepositoryTest.java
+++ chapter06/src/test/java/springbootdeveloper/MemberRepositoryTest.java
@@ -1,7 +1,7 @@
 package springbootdeveloper;
 
-import me.ahngeunsu.springbootdeveloper.Member;
-import me.ahngeunsu.springbootdeveloper.MemberRepository;
+import me.ko.springbootdeveloper.Member;
+import me.ko.springbootdeveloper.MemberRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
