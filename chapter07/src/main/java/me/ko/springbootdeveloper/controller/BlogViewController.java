1bef21a78f2452b5a6b2d48f4b1481e6c7d54082
kohyeokjea <kohj0686@naver.com>
2025-01-13 21:44:13 +0900

    feat: chapter07 CRUD and View

--- chapter07/src/main/java/me/ko/springbootdeveloper/controller/BlogViewController.java
+++ chapter07/src/main/java/me/ko/springbootdeveloper/controller/BlogViewController.java
@@ -1,11 +1,15 @@
 package me.ko.springbootdeveloper.controller;
 
 import lombok.RequiredArgsConstructor;
+import me.ko.springbootdeveloper.domain.Article;
 import me.ko.springbootdeveloper.dto.ArticleListViewResponse;
+import me.ko.springbootdeveloper.dto.ArticleViewResponse;
 import me.ko.springbootdeveloper.service.BlogService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -23,4 +27,29 @@ public class BlogViewController {
 
         return "articleList";   // -> 이거 다음에 만들어야될 파일의 위치 및 파일명
     }
+
+    @GetMapping("/articles/{id}")
+    public String getArticlae(@PathVariable Long id, Model model) {
+        Article article = blogService.findById(id);
+        model.addAttribute("article", new ArticleViewResponse(article));
+
+        return "article";
+    }
+    /*
+        templates -> article.html을 생성
+     */
+
+    @GetMapping("/new-article")
+    // 1. id 키를 가진 쿼리 파라미터의 값을 id 변수에 매핑(id는 없을 수도 있음)
+    public String newArticle(@RequestParam(required = false) Long id, Model model) {
+        if(id == null) {        // 2. id가 없으면 새로 생성
+            model.addAttribute("article", new ArticleViewResponse());
+        } else {                // 3. id가 있으면 수정
+            Article article = blogService.findById(id);
+            model.addAttribute("article", new ArticleViewResponse(article));
+        }
+
+        return "newArticle";
+    }
+
 }
